6715212e489a8422f33f8ea275fef89b1572ab02
bartlomiej <bartlomiej@euroloop.tech>
2022-09-12 22:11:41 +0200

    changes content website

--- src/views/SectionsPage/Sections/PricesLanguage/SectionPricesLanguageEnglish.js
+++ src/views/SectionsPage/Sections/PricesLanguage/SectionPricesLanguageEnglish.js
@@ -72,7 +72,7 @@ export default function SectionPricing({ ...rest }) {
                   <h4 style={{fontWeight: 'bold'}} className={classes.marginBottom30}>Oferujemy pakiet semestralny</h4>
 
                   <h1 style={{marginTop: '-20px'}} className={classes.cardTitleWhite}>
-                    40 <small>zł/h</small>
+                    45 <small>zł/h</small>
                   </h1>
                   <ul>
                     <li>
@@ -98,7 +98,7 @@ export default function SectionPricing({ ...rest }) {
                   <h4 style={{fontWeight: 'bold'}} className={classes.marginBottom30}>Oferujemy pakiet semestralny</h4>
 
                   <h1 style={{marginTop: '-20px'}} className={classes.cardTitleWhite}>
-                    25 <small>zł/h</small>
+                    30 <small>zł/h</small>
                   </h1>
                   <ul>
                     <li>
